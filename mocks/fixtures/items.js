ac417b1fb623e483cae88f74e4d458e02126b70d
Manu Guisado <1848461+mjguisado@users.noreply.github.com>
2024-12-11 18:22:15 +0100

    Eliminamos mocks-server

--- mocks/fixtures/items.js
+++
@@ -1,9 +0,0 @@
-const items = []
-for (let index = 0; index < 5; index++) {
-  items[index] = {
-    id: index,
-    name: `Item ${index}`
-  }
-}
-
-export default items
--- src/app.test.js
+++ src/app.test.js
@@ -1,5 +1,4 @@
 import { suite, test, before, after } from 'node:test'
-import { createServer } from '@mocks-server/main'
 import { build } from './app.js'
 import path from 'path'
 
@@ -9,40 +8,21 @@ suite('GET Method', () => {
     let mocksServer
 
     before(async () => {
-
-        mocksServer = createServer(
-            {
-                config: {
-                    readFile: true,
-                },
-                files: {
-                    enabled: true,
-                },
-                mock: {
-                    collections: {
-                      selected: "base"
-                    }
-                }
-            }
-        )
-        await mocksServer.start();
         fastifyServer = await build({
             logger: { level: 'info' }
         })
-
     })
 
     test('Miss', async (t) => {
         t.plan(1)
         const response = await fastifyServer.inject({
             method: 'GET',
-            url: '/mocks/items'
+            url: '/restful-api/users'
         })
         t.assert.strictEqual(response.statusCode, 200, 'returns a status code of 200')
     })
 
     after(async () => {
-        await mocksServer.stop()
     })
 
 })
