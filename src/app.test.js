40fa183c7807e90542d928aaaf8efa5f1dcb6fbc
Manu Guisado <1848461+mjguisado@users.noreply.github.com>
2024-12-11 18:53:39 +0100

    QUitado el mocks server por ser CommonJS

--- src/app.test.js
+++ src/app.test.js
@@ -1,11 +1,9 @@
 import { suite, test, before, after } from 'node:test'
 import { build } from './app.js'
-import path from 'path'
 
 suite('GET Method', () => {
 
     let fastifyServer
-    let mocksServer
 
     before(async () => {
         fastifyServer = await build({
@@ -17,12 +15,22 @@ suite('GET Method', () => {
         t.plan(1)
         const response = await fastifyServer.inject({
             method: 'GET',
-            url: '/restful-api/users'
+            url: '/restful-api/objects?id=3&id=5&id=10'
         })
         t.assert.strictEqual(response.statusCode, 200, 'returns a status code of 200')
     })
 
+    test('404', async (t) => {
+        t.plan(1)
+        const response = await fastifyServer.inject({
+            method: 'GET',
+            url: '/restful-api/objects/100'
+        })
+        t.assert.strictEqual(response.statusCode, 404, 'returns a status code of 404')
+    })
+
     after(async () => {
+        await fastifyServer.close()
     })
 
 })
