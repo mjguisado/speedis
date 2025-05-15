65934808e5b392e3c2e92ee2c3480178c39c00c8
Manu Guisado <1848461+mjguisado@users.noreply.github.com>
2025-05-15 12:49:35 +0200

    fixing the generate selft certificate scripts

--- 3rparties/keycloak/generate_self_signed_cert.sh
+++ 3rparties/keycloak/generate_self_signed_cert.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 openssl req -x509 -nodes -days 365 \
   -newkey rsa:2048 \
-  -keyout keycloak.key \
-  -out keycloak.crt \
-  -config san.cnf
-mkdir ./certs
-mv keycloak.key keycloak.crt ./certs
-
+  -keyout $DIR/keycloak.key \
+  -out $DIR/keycloak.crt \
+  -config $DIR/san.cnf
+mkdir -p $DIR/certs
+mv $DIR/keycloak.key $DIR/keycloak.crt $DIR/certs
