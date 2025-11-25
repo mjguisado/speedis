78401ffe93ef49efc1ecc52db85e5fc2910edf58
Manu Guisado <1848461+mjguisado@users.noreply.github.com>
2025-11-26 00:46:06 +0100

    Bug Fix

--- generate_self_signed_cert.sh
+++ generate_self_signed_cert.sh
@@ -2,7 +2,7 @@
 ./conf/generate_self_signed_cert.sh
 openssl x509 -in ./conf/certs/speedis.crt -noout -issuer -subject -ext subjectAltName
 ./3rparties/haproxy/generate_self_signed_cert.sh
-openssl x509 -in ./3rparties/haproxy/mocks.crt -noout -issuer -subject -ext subjectAltName
+openssl x509 -in ./3rparties/haproxy/certs/mocks.pem -noout -issuer -subject -ext subjectAltName
 ./3rparties/keycloak/generate_self_signed_cert.sh
 openssl x509 -in ./3rparties/keycloak/certs/keycloak.crt -noout -issuer -subject -ext subjectAltName
 ./mocks/generate_self_signed_cert.sh
