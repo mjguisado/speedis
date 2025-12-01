edd87a870eecd7e24d3f8e8227ec830e0768c864
Manu Guisado <1848461+mjguisado@users.noreply.github.com>
2025-12-01 17:01:15 +0100

    linux/amd64 iamges

--- uploadImages.sh
+++ uploadImages.sh
@@ -1,6 +1,6 @@
-docker build --pull -f 'Dockerfile' -t 'mjguisado/speedis:latest' '.'
+docker buildx build --pull -f 'Dockerfile' -t 'mjguisado/speedis:latest' --platform 'linux/amd64' --push '.'
+docker buildx build --pull -f 'Dockerfile.mocks' -t 'mjguisado/mocks:latest' --platform 'linux/amd64' --push '.'
+docker buildx build --pull -f 'Dockerfile.keycloak' -t 'mjguisado/keycloak:latest' --platform 'linux/amd64' --push '.'
+docker build --pull -f 'Dockerfile' -t 'mjguisado/speedis:latest' '.' --push
 docker build --pull -f 'Dockerfile.mocks' -t 'mjguisado/mocks:latest' '.'
 docker build --pull -f 'Dockerfile.keycloak' -t 'mjguisado/keycloak:latest' '.'
-docker push mjguisado/speedis:latest
-docker push mjguisado/mocks:latest
-docker push mjguisado/keycloak:latest
