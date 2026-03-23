0130129d912fc667114cdcd5880803da32cc4b09
Manu Guisado <1848461+mjguisado@users.noreply.github.com>
2026-03-23 09:56:48 +0100

    Updating dependencing

--- uploadImages.sh
+++
@@ -1,4 +0,0 @@
-docker buildx build --pull -f 'Dockerfile' -t 'mjguisado/speedis:latest' --platform 'linux/amd64' --push '.'
-docker buildx build --pull -f 'Dockerfile.mocks' -t 'mjguisado/mocks:latest' --platform 'linux/amd64' --push '.'
-docker build --pull -f 'Dockerfile' -t 'mjguisado/speedis:latest' '.' --push
-docker build --pull -f 'Dockerfile.mocks' -t 'mjguisado/mocks:latest' '.'
